4903c042c1e70af0f39f91ec40731f20e28eb590
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-06 11:24:35 +0800

    loon_bd4.js

--- loon_bd4.js
+++ loon_bd4.js
@@ -94,6 +94,6 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost:ssdklog.cloud.play.cn:14508\r\nConnection: keep-alive\r\nProxy-Connection: keep-alive\r\nAccept: */*\r\n\r\n`
+    var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost:${conHost})ssdklog.cloud.play.cn:14507\r\nConnection: keep-alive\r\nProxy-Connection: keep-alive\r\nAccept: */*\r\n\r\n`
     $tunnel.write($session, header)
 }
