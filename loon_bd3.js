f18dc184a3d30f84c925e2809848623fceda2077
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-20 22:47:37 +0800

    loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -94,6 +94,6 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\ncloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\nclientVersion: 8.4.1\r\nHost: ${conHost}\r\nConnection: keep-alive\r\nProxy-Connection: keep-alive\r\nAccept: */*\r\nUser-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n\r\n`
+    var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\ncloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\nclientVersion: 8.4.1\r\nHost: ${conHost}\r\nConnection: keep-alive\r\nProxy-Connection: keep-alive\r\nAccept: */*\r\nUser-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\nX-T5-Auth: "1962898709\r\n\r\n`
     $tunnel.write($session, header)
 }
