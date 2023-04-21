05ee76197795ceebe2c845fae9650c3131f5ec89
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-21 17:39:23 +0800

    loon_bd4.js

--- loon_bd4.js
+++ loon_bd4.js
@@ -1,36 +1,37 @@
-```javascript
+/** 
+* 实现HTTP代理协议，可用于Loon的自定义协议（custom类型） 
+* 使用方式： 
+* [Proxy] 
+* customHttp = custom, remoteAddress, port, script-path=https://raw.githubusercontent.com/Loon0x00/LoonExampleConfig/master/Script/http.js 
+*/
+
 // HTTP请求状态
 const HTTP_STATUS = {
   INVALID: -1,
   CONNECTED: 0,
   WAITRESPONSE: 1,
-  FORWARDING: 2,
-  CONNECTING_TLS: 3,
-  CONNECTED_TLS: 4
+  FORWARDING: 2
 }
 
 let httpStatus = HTTP_STATUS.INVALID
-let tlsClient = null
 
 function tunnelDidConnected() {
   console.log($session)
-
+  
   if ($session.proxy.isTLS) {
     // HTTPS
-    httpStatus = HTTP_STATUS.CONNECTING_TLS
-    tlsClient = $tls.createClient({ host: $session.sni })
-    tlsClient.connect()
+    _writeHttpsHeader()
   } else {
     // HTTP
     _writeHttpHeader()
     httpStatus = HTTP_STATUS.CONNECTED
   }
-
+  
   return true
 }
 
 function tunnelTLSFinished() {
-  _writeHttpHeader()
+  _writeHttpsHeader()
   httpStatus = HTTP_STATUS.CONNECTED
   return true
 }
@@ -44,8 +45,10 @@ function tunnelDidRead(data) {
       httpStatus = HTTP_STATUS.FORWARDING
       $tunnel.established($session)
       return null // Do not forward data to client
+      
     case HTTP_STATUS.FORWARDING:
       return data
+      
     default:
       return null
   }
@@ -58,10 +61,7 @@ function tunnelDidWrite() {
       httpStatus = HTTP_STATUS.WAITRESPONSE
       $tunnel.readTo($session, "\x0D\x0A\x0D\x0A") // Read remote data until "\r\n\r\n"
       return false // Interrupt write callback
-    case HTTP_STATUS.CONNECTED_TLS:
-      _writeHttpsHeader() // TLS握手结束后发送HTTP请求头
-      httpStatus = HTTP_STATUS.CONNECTED
-      return true
+      
     default:
       return true
   }
@@ -75,25 +75,13 @@ function tunnelDidClose() {
 function _writeHttpHeader() {
   const conHost = $session.conHost
   const conPort = $session.conPort
-
-  const header =
-    `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n` +
-    `Host:${conHost}\r\n` +
-    `Connection: keep-alive\r\n` +
-    `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n` +
-    `cloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\n` +
-    `Proxy-Connection: keep-alive\r\n\r\n`
-
+  
+  const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
+               + `Host:gz189cloud2.oos-gz.ctyunapi.cn:${conHost}\r\n`
+               + `Connection: keep-alive\r\n`
+               + `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n`
+               + `cloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\n`
+               + `Proxy-Connection: keep-alive\r\n\r\n`
+               
   $tunnel.write($session, header)
-}
-
-function _writeHttpsHeader() {
-
-  const header =
-    `GET ${$session.sni}/ HTTP/1.1\r\n` +
-    `Host: gz189cloud2.oos-gz.ctyunapi.cn\r\n` +
-    `cloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\n` +
-    `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n` +
-    `Connection: keep-alive\r\n\r\n`
-  tlsClient.write(header)
 }
