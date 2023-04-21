5de79870df51f7e312519b9cff05e1ebb5be88d7
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-21 13:48:15 +0800

    loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -1,37 +1,36 @@
-/** 
-* 实现HTTP代理协议，可用于Loon的自定义协议（custom类型） 
-* 使用方式： 
-* [Proxy] 
-* customHttp = custom, remoteAddress, port, script-path=https://raw.githubusercontent.com/Loon0x00/LoonExampleConfig/master/Script/http.js 
-*/
-
+```javascript
 // HTTP请求状态
 const HTTP_STATUS = {
   INVALID: -1,
   CONNECTED: 0,
   WAITRESPONSE: 1,
-  FORWARDING: 2
+  FORWARDING: 2,
+  CONNECTING_TLS: 3,
+  CONNECTED_TLS: 4
 }
 
 let httpStatus = HTTP_STATUS.INVALID
+let tlsClient = null
 
 function tunnelDidConnected() {
   console.log($session)
-  
+
   if ($session.proxy.isTLS) {
     // HTTPS
-    _writeHttpsHeader()
+    httpStatus = HTTP_STATUS.CONNECTING_TLS
+    tlsClient = $tls.createClient({ host: $session.sni })
+    tlsClient.connect()
   } else {
     // HTTP
     _writeHttpHeader()
     httpStatus = HTTP_STATUS.CONNECTED
   }
-  
+
   return true
 }
 
 function tunnelTLSFinished() {
-  _writeHttpsHeader()
+  _writeHttpHeader()
   httpStatus = HTTP_STATUS.CONNECTED
   return true
 }
@@ -45,10 +44,8 @@ function tunnelDidRead(data) {
       httpStatus = HTTP_STATUS.FORWARDING
       $tunnel.established($session)
       return null // Do not forward data to client
-      
     case HTTP_STATUS.FORWARDING:
       return data
-      
     default:
       return null
   }
@@ -61,7 +58,10 @@ function tunnelDidWrite() {
       httpStatus = HTTP_STATUS.WAITRESPONSE
       $tunnel.readTo($session, "\x0D\x0A\x0D\x0A") // Read remote data until "\r\n\r\n"
       return false // Interrupt write callback
-      
+    case HTTP_STATUS.CONNECTED_TLS:
+      _writeHttpsHeader() // TLS握手结束后发送HTTP请求头
+      httpStatus = HTTP_STATUS.CONNECTED
+      return true
     default:
       return true
   }
@@ -75,17 +75,23 @@ function tunnelDidClose() {
 function _writeHttpHeader() {
   const conHost = $session.conHost
   const conPort = $session.conPort
-  
-  const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
-               + `Host:${conHost}\r\n`
-               + `Connection: keep-alive\r\n`
-               + `User-Agent: Cloud5G/3.7.0 (iPhone: ios 15.1: Scale/3.00) baiduboxapp\r\n`
-               + `X-T5-Auth: 1962898709\r\n`
-               + `Proxy-Connection: keep-alive\r\n\r\n`
-               
+
+  const header =
+    `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n` +
+    `Host:${conHost}\r\n` +
+    `Connection: keep-alive\r\n` +
+    `User-Agent: Cloud5G/3.7.0 (iPhone: ios 15.1: Scale/3.00) baiduboxapp\r\n` +
+    `X-T5-Auth: 1962898709\r\n` +
+    `Proxy-Connection: keep-alive\r\n\r\n`
+
   $tunnel.write($session, header)
 }
 
 function _writeHttpsHeader() {
-  // TODO: Add HTTPS header
+  const header =
+    `GET / HTTP/1.1\r\n` +
+    `Host: ${$session.sni}\r\n` +
+    `Connection: keep-alive\r\n\r\n`
+  tlsClient.write(header)
 }
+```
