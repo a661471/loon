8aa7dd60c38ced3d6d453f2005f7e888eb1c33bb
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-21 17:12:12 +0800

    loon_bd4.js

--- loon_bd4.js
+++ loon_bd4.js
@@ -88,12 +88,9 @@ function _writeHttpHeader() {
 }
 
 function _writeHttpsHeader() {
-  const conHost = $session.conHost
-  const conPort = $session.conPort
-  const conUrl = $session.conUrl
 
   const header =
-    `GET http://${conHost}/${conUrl} HTTP/1.1\r\n` +
+    `GET ${$session.sni}/ HTTP/1.1\r\n` +
     `Host: gz189cloud2.oos-gz.ctyunapi.cn\r\n` +
     `cloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\n` +
     `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n` +
