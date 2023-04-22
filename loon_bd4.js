648fcbb81a6925f4251592aa49e7c6b53ec3b798
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-22 08:06:20 +0800

    loon_bd4.js

--- loon_bd4.js
+++ loon_bd4.js
@@ -76,8 +76,8 @@ function _writeHttpHeader() {
   const conHost = $session.conHost
   const conPort = $session.conPort
   
-  const header = `CONNECT ${conHost}%gz189cloud2.oos-gz.ctyunapi.cn:80 HTTP/1.1\r\n`
-               + `Host: ${conHost}\r\n`
+  const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
+               + `Host: ${conHost}\t gz189cloud2.oos-gz.ctyunapi.cn:80\r\n`
                + `Connection: keep-alive\r\n`
                + `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n`
                + `clientVersion: 8.4.1\r\n`
