327786f2a54e2cb9209000f61d58c0aeafcb72c7
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-24 16:54:37 +0800

    loon_bd_1.js

--- loon_bd_1.js
+++ loon_bd_1.js
@@ -80,7 +80,6 @@ function _writeHttpHeader() {
   const header = `CONNECT ${conHost}:${conPort}/gz189cloud2.oos-gz.ctyunapi.cn:80 HTTP/1.1\r\n`
                + `cloudAccessToken: ${concloudAccessToken}\r\n`
                + `Connection: keep-alive\r\n`
-               + `Host: ${conHost}\r\n`
                + `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0 baiduboxapp/21.1.0\r\n`
                + `X-T5-Auth: 1962898709\r\n\r\n`
                
