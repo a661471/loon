f2f3053bc5d1798bad26aa320bff94a1d8d7a2c6
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-25 17:32:46 +0800

    loon_bd_1.js

--- loon_bd_1.js
+++ loon_bd_1.js
@@ -1,50 +1,87 @@
-function tunnelDidConnected(httpStatus) {
-    console.log($session);
-    if ($session.proxy.isTLS) {
-      // https
-    } else {
-      // http
-      writeHttpHeader(httpStatus);
-    }
+/** 
+* 实现HTTP代理协议，可用于Loon的自定义协议（custom类型） 
+* 使用方式： 
+* [Proxy] 
+* customHttp = custom, remoteAddress, port, script-path=https://raw.githubusercontent.com/Loon0x00/LoonExampleConfig/master/Script/http.js 
+*/
+
+// HTTP请求状态
+const HTTP_STATUS = {
+  INVALID: -1,
+  CONNECTED: 0,
+  WAITRESPONSE: 1,
+  FORWARDING: 2
 }
 
-function tunnelTLSFinished(httpStatus) {
-    writeHttpHeader(httpStatus);
+let httpStatus = HTTP_STATUS.INVALID
+
+function tunnelDidConnected() {
+  console.log($session)
+  
+  if ($session.proxy.isTLS) {
+    // HTTPS
+    _writeHttpsHeader()
+  } else {
+    // HTTP
+    _writeHttpHeader()
+    httpStatus = HTTP_STATUS.CONNECTED
+  }
+  
+  return true
 }
 
-function tunnelDidRead(data, httpStatus) {
-    if (httpStatus === 1) {
-        // check http response code == 200
-        // Assume success here
-        console.log("http handshake success");
-        httpStatus = 2;
-        $tunnel.established($session); // can forwarding data
-        return null;
-    }
-    //直接返回数据
-    return data;
+function tunnelTLSFinished() {
+  _writeHttpsHeader()
+  httpStatus = HTTP_STATUS.CONNECTED
+  return true
 }
 
-function tunnelDidWrite(httpStatus) {
-    if (httpStatus === 0) {
-        console.log("write http head success");
-        httpStatus = 1;
-        $tunnel.readTo($session, "\x0D\x0A\x0D\x0A"); // read remote data until \r\n\r\n
-        return false; // interrupt write callback
-    }
-    return true;
+function tunnelDidRead(data) {
+  switch (httpStatus) {
+    case HTTP_STATUS.WAITRESPONSE:
+      // Check HTTP response code == 200 (or other codes)
+      // Assume success here
+      console.log("HTTP handshake success")
+      httpStatus = HTTP_STATUS.FORWARDING
+      $tunnel.established($session)
+      return null // Do not forward data to client
+      
+    case HTTP_STATUS.FORWARDING:
+      return data
+      
+    default:
+      return null
+  }
+}
+
+function tunnelDidWrite() {
+  switch (httpStatus) {
+    case HTTP_STATUS.CONNECTED:
+      console.log("Write HTTP CONNECT header success")
+      httpStatus = HTTP_STATUS.WAITRESPONSE
+      $tunnel.readTo($session, "\x0D\x0A\x0D\x0A") // Read remote data until "\r\n\r\n"
+      return false // Interrupt write callback
+      
+    default:
+      return true
+  }
 }
 
 function tunnelDidClose() {
-    return true;
+  return true
 }
 
-function writeHttpHeader(httpStatus) {
-    const conHost = $session.conHost;
-    const conPort = $session.conPort;
-    const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost:${conHost}\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 APP/91chengguo-iOS APPVersion/9.2.2 Ecloud/9.2.2 iOS/15.1 clientId/ecloud clientModel/iPhone proVersion/1.0.0\r\nX-T5-Auth: YTY0Nzlk\r\nAccess-Control-Allow-Headers: X-Requested-With, Authorization, Content-Type, X-H5App-ID, X-H5App-Timestamp, X-H5App-Signature, X-H5App-OS, X-H5App-Client, X-H5App-SDK, X-H5App-JSAPI, X-H5App-Device, X-H5App-Token\r\nProxy-Connection: keep-alive\r\n\r\n`;
-    $tunnel.write($session, header);
+// Helpers
+function _writeHttpHeader() {
+  const conHost = $session.conHost
+  const conPort = $session.conPort
+  
+  const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
+               + `User-Agent: %E5%A4%A9%E7%BF%BC%E8%A7%86%E8%AE%AF/1.31.1.66 CFNetwork/1325.0.1 Darwin/21.1.0\r\n`
+               + `Host: user.jpush.cn\r\n`
+               + `Content-Type: text/plain\r\n`
+               + `Connection: keep-alive\r\n`
+               + `Authorization: Basic Njk5MjIzODg4Njk6OTEyYjcwYzNkZDcyMTZhZTA2MmM2ZTkwOTNjMjg0OGI=\r\n\r\n`
+               
+  $tunnel.write($session, header)
 }
-
-// entry point
-tunnelDidConnected(HTTP_STATUS_INVALID);
