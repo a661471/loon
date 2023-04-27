9e2cea589c0e6931be500207bb08cd8d3ff8dde1
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-27 21:03:46 +0800

    更新 loon_bd5.js

--- loon_bd5.js
+++ loon_bd5.js
@@ -94,6 +94,6 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort}a.189.cn HTTP/1.1\r\nHost:${conHost}\r\nConnection: keep-alive\r\nUser-Agent: CtClient;10.1.0;iOS;15.1;iPhone 13 Pro Max;NjAwMjA0!#!MTgwMjk=&604.1 baiduboxapp\r\nX-T5-Auth: YTY0Nzlk\r\nProxy-Connection: keep-alive\r\n\r\n`
+    var header = `CONNECT ${conHost}:${conPort}@reportappgo.189.cn:8443 HTTP/1.1\r\nHost:${conHost}\r\nUser-Agent: CTPocket/10.20.0 (iPhone; iOS13.7; Scale/2.00)\r\nConnection: keep-alive\r\nProxy-Connection: keep-alive\r\n\r\n`
     $tunnel.write($session, header)
 }
