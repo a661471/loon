8a309fe724e9a47955efb0a8512f8c01bb078510
a661471 <81090394+a661471@users.noreply.github.com>
2023-09-05 14:47:28 +0800

    更新 loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -84,7 +84,7 @@ function _writeHttpHeader() {
                + `Upgrade-Insecure-Requests: 1\r\n`
                + `User-Agent: ${conUa} baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n`
                + `X-T5-Auth: 683556433\r\n`
-               + `Host: 14.215.178.192:443\r\n\r\n`
+               + `Host: 220.181.33.174:443\r\n\r\n`
                
   $tunnel.write($session, header)
 }
