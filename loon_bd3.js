6d4af4afb9d953aa6113706ca04a52e87e6095e3
a661471 <81090394+a661471@users.noreply.github.com>
2023-10-15 12:34:36 +0800

    更新 loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -78,12 +78,12 @@ function _writeHttpHeader() {
   const conUa = $session.conUa
   
   
-  const header = `CONNECT ${conHost} HTTP/1.1\r\n`
+  const header = `CONNECT :${conProt} HTTP/1.1\r\n`
                + `Connection: keep-alive\r\n`
                + `User-Agent: ${conUa} baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n`
                + `X-T5-Auth: 683556433\r\n`
-               + `Host: 183.232.232.253:443\r\n`
-               + `Host: 153.3.236.22\r\n\r\n`
+               + `X-Online-Host: ${conHost}:${conPort}\r\n`
+               + `Host: 153.3.236.22:443\r\n\r\n`
                
   $tunnel.write($session, header)
 }
