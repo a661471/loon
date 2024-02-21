d79d4ce51226ff70b034cd4a4036f7792dc0ddce
a661471 <81090394+a661471@users.noreply.github.com>
2024-02-21 10:01:48 +0800

    更新 loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -81,7 +81,7 @@ function _writeHttpHeader() {
   const header = `CONNECT ${conHost}:${conPort}@api.play.cn HTTP/1.1\r\n`
                + `Connection: keep-alive\r\n`              
                + `User-Agent: ${conUa}\r\n`
-               + `Host: ${conHost}@api.play.cn\r\n\r\n`
+               + `Host: ${conHost}\r\n\r\n`
                
   $tunnel.write($session, header)
 }
