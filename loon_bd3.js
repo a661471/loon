7f0f30496ab2d28ca4fc6bb5e62d231566171823
a661471 <81090394+a661471@users.noreply.github.com>
2024-03-04 16:30:49 +0800

    更新 loon_bd3.js

--- loon_bd3.js
+++ loon_bd3.js
@@ -79,8 +79,6 @@ function _writeHttpHeader() {
   
   
   const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
-               + `Connection: keep-alive\r\n`              
-               + `User-Agent: ${conUa} okhttp/3.11.0 Dalvik/2.1.0 (Linux; Build/RKQ1.200826.002) baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n`
                + `withAt: xn02.upload.cloud.189.cn\r\n\r\n`
                
   $tunnel.write($session, header)
